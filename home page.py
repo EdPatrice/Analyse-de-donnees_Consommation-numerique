b9bf739bb98f07fa255bf86fc8c4f1fdf8376adb
EdPatrice <paveluspatrice@gmail.com>
2025-04-03 12:03:49 -0400

    Update home page.py

--- home page.py
+++ home page.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
 # Set the page title and icon
-st.set_page_config(page_title="Report", page_icon="🚀")
+st.set_page_config(page_title="Report", page_icon="🚀", layout="wide")
 
 # Display something on the page
-st.title("Welcome to the numerical consumption report!")
+st.title("Rapport de consommation numérique")
+st.markdown("<br><br>", unsafe_allow_html=True)
+st.markdown("***", unsafe_allow_html=True)
+st.markdown("# Membres du groupe")
+st.markdown("1. Ed Patrice Wakfield ***Pavelus*** \n2. Alissa ***Jean Pierre*** \n3. Jacky ***Guilhomme*** \n4. Marly ***Richelieu***")
